22ad99001d23c62bd1fd0c3404f041d283e02835
NightKirie <qwer55113322@gmail.com>
2018-11-01 19:47:28 +0800

    Update practice1.c

--- function/practice1.c
+++ function/practice1.c
@@ -1,13 +1,1 @@
-#include<stdio.h>
-int power(int x, int n){
-    int ans = 1;
-    for(int i = 0; i < n; ++i)
-        ans *= x; 
-    return ans;
-}
-int main(){
-    int x, n, ans;
-    scanf("%d%d", &x, &n);
-    ans = power(x, n);
-    printf("%d\n", ans);
-}
+
